3a46734a8af25f91abfd364517a8444c105e70a2
Yisui Hu <hu@crafting.dev>
2022-09-03 01:05:18 +0000

    update base image

--- shared/snapshots/base.sh
+++ shared/snapshots/base.sh
@@ -7,6 +7,8 @@ function fatal() {
 
 [[ "$(id -u)" == "0" ]] || fatal "Must run as root!"
 
+apt-get update
+apt-get install -y iputils-ping iputils-tracepath redis-tools mysql-client postgresql-client
 curl -sSLf https://dl.k8s.io/v1.20.5/kubernetes-client-linux-amd64.tar.gz | tar -C /usr/local/bin -zx --strip-components=3
 curl -sSLf https://releases.hashicorp.com/terraform/1.2.8/terraform_1.2.8_linux_amd64.zip | funzip >/usr/local/bin/terraform
 chmod a+rx /usr/local/bin/terraform
