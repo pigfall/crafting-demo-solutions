8eaa6671652b73f094834bb9a872fc6f68e2d0b8
Zongze Tang <tangbe9@gmail.com>
2022-10-09 04:04:34 +0000

    get exact ecs task ip

--- apps/aws-notebook/tf/get-task-ip.sh
+++ apps/aws-notebook/tf/get-task-ip.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
+set -o pipefail
 
 AWS_REGION=$(aws configure get region)
 
 TASK_ARN=$(aws ecs list-tasks --region ${AWS_REGION} --cluster ${ECS_CLUSTER_NAME} --service=notebook_${SANDBOX_ID} | jq .taskArns[0] | tr -d '"')
 TASK_IP=$(aws ecs describe-tasks --tasks ${TASK_ARN} --region ${AWS_REGION} --cluster ${ECS_CLUSTER_NAME} | jq '.tasks[0].attachments[0].details[] | select(.name=="privateIPv4Address") | .value' |  tr -d '"')
 
-echo {'"'task_private_ip'"':${TASK_IP}}
+echo {TASK_IP}
--- apps/aws-notebook/tf/on_create.sh
+++ apps/aws-notebook/tf/on_create.sh
@@ -17,3 +17,26 @@ terraform apply -auto-approve \
   -var="ssh_public_key=${SSH_PUBLIC_KEY}" \
   -var="service_launch_type=${SERVICE_LAUNCH_TYPE}" \
   -var="task_image=${TASK_IMAGE}"
+
+
+while true;do
+  TASK_IP=$(./get-task-ip.sh)
+  if [[ -z "${FINAL_TASK_IP}" ]];then
+    FINAL_TASK_IP=${TASK_IP}
+    sleep 2
+    continue
+  fi
+  if [[ "$TASK_IP"=="${FINAL_TASK_IP}" ]];then
+     FINAL_TASK_IP_COUNT=$(expr $FINAL_TASK_IP_COUNT + 1)
+     if [[ $FINAL_TASK_IP_COUNT -ge 3]];then
+       break
+     fi
+  else
+    FINAL_TASK_IP_COUNT=$TASK_IP
+    FINAL_TASK_IP_COUNT=0
+  fi
+  sleep 2
+done
+
+mkdir -p ~/.aws-notebook
+echo ${FINAL_TASK_IP} > ~/.aws-notebook/ip.txt
