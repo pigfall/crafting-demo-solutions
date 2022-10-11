59c600bc878aadb9df8c90cc01c094b456975101
Zongze Tang <tangbe9@gmail.com>
2022-10-11 05:53:25 +0000

    output json

--- apps/aws-notebook/tf/on_create.sh
+++ apps/aws-notebook/tf/on_create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -e
 set -o pipefail
 
 # Prepare key-pair used to connect to ecs container
@@ -9,11 +9,13 @@ rm -f ~/.ssh/aws-notebook_rsa.pub
 ssh-keygen -t rsa -N "" -f ~/.ssh/aws-notebook_rsa
 SSH_PUBLIC_KEY=$(cat ~/.ssh/aws-notebook_rsa.pub)
 
-terraform init 
+terraform init >&2
 terraform apply -auto-approve \
   -var="ecs_cluster_name=${ECS_CLUSTER_NAME}" \
   -var="subnet_id=${SUBNET_ID}" \
   -var="security_groups=${SECURITY_GROUPS}" \
   -var="ssh_public_key=${SSH_PUBLIC_KEY}" \
   -var="service_launch_type=${SERVICE_LAUNCH_TYPE}" \
-  -var="task_image=${TASK_IMAGE}"
+  -var="task_image=${TASK_IMAGE}" >&2
+
+terraform output -json
