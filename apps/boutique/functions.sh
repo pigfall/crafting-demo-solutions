a216325aed92565e6a436859e66e8f14f71cf94f
Yisui Hu <hu@crafting.dev>
2022-10-22 00:52:15 +0000

    allow namespace override

--- apps/boutique/functions.sh
+++ apps/boutique/functions.sh
@@ -6,6 +6,6 @@ function fatal() {
 APP_DIR="$(readlink -nf ${BASH_SOURCE[0]%/*})"
 : ${SRC_DIR:="$(readlink -nf $APP_DIR/../../../demo)"}
 K8S_MANIFEST_FILE="$SRC_DIR/release/kubernetes-manifests.yaml"
-NAMESPACE="${SANDBOX_APP}-${SANDBOX_ID}"
+NAMESPACE="${K8S_NS:-${SANDBOX_APP}-${SANDBOX_ID}}"
 
 [[ -f "$K8S_MANIFEST_FILE" ]] || fatal "Not found: $K8S_MANIFEST_FILE"
